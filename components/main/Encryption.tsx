4363ab6a9f6a5c941b58d35e050d24cdc4aa7a3e
JoyceTimothyEngels <joyce.t.engels@gmail.com>
2023-11-26 13:44:49 +0100

    Footer und Btn link

--- components/main/Encryption.tsx
+++ components/main/Encryption.tsx
@@ -7,8 +7,10 @@ import Image from "next/image";
 
 const Encryption = () => {
   return (
+    
     <div className="flex flex-row relative items-center justify-center min-h-screen w-full h-full">
-      <div className="absolute w-auto h-auto top-0 z-[5]">
+      <div className="absolute w-auto h-auto top-0 z-[5]" id="contact">
+        
         <motion.div
           variants={slideInFromTop}
           className="text-[40px] font-medium text-center text-gray-200"
@@ -22,33 +24,53 @@ const Encryption = () => {
 
       <div className="flex flex-col items-center justify-center translate-y-[-50px] absolute z-[20] w-auto h-auto">
         <div className="flex flex-col items-center group cursor-pointer w-auto h-auto">
-          <Image
+        <div className="flex flex-row gap-5">
+          {/* <Image
             src="/LockTop.png"
             alt="Lock top"
             width={50}
             height={50}
             className="w-[50px] translate-y-5 transition-all duration-200 group-hover:translate-y-11"
-          />
+          /> */}
+          <a href="https://de.linkedin.com/in/joyce-timothy-engels-215173276" className="cursor-pointer">
           <Image
-            src="/LockMain.png"
-            alt="Lock Main"
-            width={70}
-            height={70}
+            src="/linkedin-icon-2.svg"
+            alt="LinkedIn"
+            width={90}
+            height={90}
             className=" z-10"
           />
-          
+          </a>
+          <a href="mailto:info@engels-development.com" className="cursor-pointer">
+          <Image
+            src="/mail-icon.png"
+            alt="Lock Main"
+            width={90}
+            height={90}
+            className=" z-10"
+          />
+          </a>
+          <a href="https://www.instagram.com/engels_development/" className="cursor-pointer">
+          <Image
+            src="/5296765_camera_instagram_instagram logo_icon.png"
+            alt="Lock Main"
+            width={90}
+            height={90}
+            className=" z-10"
+          />
+          </a>
         </div>
 
-         <div className="Welcome-box px-[15px] py-[4px] z-[20] brder my-[20px] border-[#7042f88b] opacity-[0.9]">
+         {/* <div className="Welcome-box px-[15px] py-[4px] z-[20] brder my-[20px] border-[#7042f88b] opacity-[0.9]">
           <h1 className="Welcome-text text-[12px]">Encryption</h1>
         </div>
       </div>
       <div className="absolute z-[20] bottom-[10px] px-[5px]">
         <div className="text-[20px] font-medium text-center text-gray-300">
           Secure your data with end-to-end encryption
-        </div> 
+        </div>  */}
       </div>
-
+        </div>
       <div className="w-full flex items-start justify-center absolute">
         <video
           loop
--- components/main/Footer.tsx
+++ components/main/Footer.tsx
@@ -17,54 +17,54 @@ const Footer = () => {
                 
 
                 <div className="min-w-[200px] h-auto flex flex-col items-center justify-start">
-                    <div className="font-bold text-[16px]">Community</div>
+                    <div className="font-bold text-[16px]">Contact</div>
                     <p className="flex flex-row items-center my-[15px] cursor-pointer">
-                        <FaYoutube />
-                        <span className="text-[15px] ml-[6px]">Youtube</span>    
+                        
+                        <span className="text-[15px] ml-[6px]">Adresse</span>    
                     </p>
                     <p className="flex flex-row items-center my-[15px] cursor-pointer">
-                        <RxGithubLogo />
-                        <span className="text-[15px] ml-[6px]">Github</span>    
+                        
+                        <span className="text-[15px] ml-[6px]">Tel</span>    
                     </p>
                     <p className="flex flex-row items-center my-[15px] cursor-pointer">
-                        <RxDiscordLogo />
-                        <span className="text-[15px] ml-[6px]">Discord</span>    
+                        
+                        <span className="text-[15px] ml-[6px]">info@engels-development.com</span>    
                     </p>
                 </div>
                 <div className="min-w-[200px] h-auto flex flex-col items-center justify-start">
                     <div className="font-bold text-[16px]">Social Media</div>
                     <p className="flex flex-row items-center my-[15px] cursor-pointer">
-                        <FaYoutube />
-                        <span className="text-[15px] ml-[6px]">Instagram</span>    
+                        <RxLinkedinLogo />
+                        <span className="text-[15px] ml-[6px]">Joyce Timothy Engels</span>    
+                    </p>
+                    <p className="flex flex-row items-center my-[15px] cursor-pointer">
+                        <RxInstagramLogo />
+                        <span className="text-[15px] ml-[6px]">engels_development</span>    
                     </p>
                     <p className="flex flex-row items-center my-[15px] cursor-pointer">
                         <RxGithubLogo />
-                        <span className="text-[15px] ml-[6px]">Twitter</span>    
-                    </p>
-                    <p className="flex flex-row items-center my-[15px] cursor-pointer">
-                        <RxDiscordLogo />
-                        <span className="text-[15px] ml-[6px]">Linkedin</span>    
+                        <span className="text-[15px] ml-[6px]">JoyceTimothyEngels</span>    
                     </p>
                 </div>
                 <div className="min-w-[200px] h-auto flex flex-col items-center justify-start">
-                    <div className="font-bold text-[16px]">About</div>
+                    
                    <p className="flex flex-row items-center my-[15px] cursor-pointer">
                      
-                        <span className="text-[15px] ml-[6px]">Become Sponsor</span>    
+                        <span className="text-[15px] ml-[6px]">Impressum</span>    
                     </p>
                     <p className="flex flex-row items-center my-[15px] cursor-pointer">
                       
-                        <span className="text-[15px] ml-[6px]">Learning about me</span>    
+                        <span className="text-[15px] ml-[6px]">Datenschutz</span>    
                     </p>
                     <p className="flex flex-row items-center my-[15px] cursor-pointer">
                   
-                        <span className="text-[15px] ml-[6px]">mifwebchain@gmail.com</span>    
+                        <span className="text-[15px] ml-[6px]">NUTZUNGSBEDINUNGEN</span>    
                     </p>
                 </div>
             </div>
 
             <div className="mb-[20px] text-[15px] text-center">
-                &copy; WebChain Dev 2023 Inc. All rights reserved
+                &copy; Engels development 2023 Inc. All rights reserved
             </div>
         </div>
     </div>
